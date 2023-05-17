94ef4de0e59841030492514ea76dc985a07be22f
buaabuaazk <2447230029@qq.com>
2023-05-17 15:38:41 +0800

    add1

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,5 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+
   <router-view/>
 </template>
 
@@ -12,7 +9,7 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #50362c;
 }
 
 nav {
